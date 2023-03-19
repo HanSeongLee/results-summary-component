96398fa294ab40c707f806140e5fc4a70c1b5507
HanSeongLee <jkhanseong@gmail.com>
2023-03-19 19:08:49 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import styles from './style.module.scss';
+import Container from 'components/Container';
+import ResultSummaryContainer from 'containers/ResultSummaryContainer';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
+    return (
+        <>
+            <header>
+                <h1 className={styles.hidden}>
+                    Results Summary Component
+                </h1>
+            </header>
+            <main className={styles.main}>
+                <Container className={styles.container}>
+                    <ResultSummaryContainer />
+                </Container>
+            </main>
+        </>
+    );
 }
 
 export default Home
