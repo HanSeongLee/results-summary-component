a3340bfca63c6b9abe0884bb46e8f0df56be219d
HanSeongLee <jkhanseong@gmail.com>
2023-03-19 18:14:21 +0900

    ADD | Button component

---
+++ components/Button/index.tsx
@@ -0,0 +1,26 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import styles from './style.module.scss';
+import cn from 'classnames';
+
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant: 'primary';
+}
+
+const Button: React.FC<IProps> = ({ variant, className, children, ...props }) => {
+    return (
+        <button className={cn(styles.button, {
+            [styles.primary]: variant === 'primary',
+        }, className)}
+                type={'button'}
+                {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+Button.defaultProps = {
+    variant: 'primary',
+};
+
+export default Button;
